b148898ba1f01e7ea3dcad6f918c9f73c0b84bfb
Marc Schaar <m-schaar@gmx.net>
2024-09-20 11:34:09 +0200

    contatcs improvments

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,26 +1,35 @@
 let ordertContacts = [];
 
 async function initContacts() {
-  let contacts = await getData("contacts");
-  let keys = Object.keys(contacts);
-  for (let index = 0; index < keys.length; index++) {
-    const key = keys[index];
-    let contactName = contacts[key].name;
-    ordertContacts.push(contactName);
-    // containerRef.innerHTML += contactName;
-  }
-  console.log(ordertContacts);
-  ordertContacts.sort();
-  console.log(ordertContacts);
+  await getContacts();
+  orderContacts();
   displayContacts();
 }
 
+async function getContacts() {
+  const contacts = await getData("contacts");
+  const keys = Object.keys(contacts);
+  for (let index = 0; index < keys.length; index++) {
+    const key = keys[index];
+    const contact = contacts[key];
+    ordertContacts.push(contact);
+  }
+}
+
+function orderContacts() {
+  ordertContacts.sort((a, b) => a.name.localeCompare(b.name));
+}
+
 function displayContacts() {
-  let containerRef = document.getElementById("contacts-container");
+  const containerRef = document.getElementById("contacts-container");
   containerRef.innerHTML = "";
   for (let i = 0; i < ordertContacts.length; i++) {
-    const contact = ordertContacts[i];
-    containerRef.innerHTML += contact;
-    console.log(contact);
+    const contactName = ordertContacts[i].name;
+    const contactEmail = ordertContacts[i].email;
+    containerRef.innerHTML += `<div class="d-flex contact">
+  <div class="user-circle center">AM</div>
+  <span class="column"><span>${contactName}</span> <a href="mailto:${contactEmail}">${contactEmail}</a></span>
+</div>
+`;
   }
 }
