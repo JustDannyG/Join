161ad4c41a3cb44f00ea4aea1f79cc275091fc87
Marc Schaar <m-schaar@gmx.net>
2024-09-20 09:19:45 +0200

    order Contacts alphabetic

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,12 +1,26 @@
-async function initContacts(){
-    let containerRef = document.getElementById("contacts-container")
-     containerRef.innerHTML=""
-    let contacts = await getData("contacts")
-    let keys = Object.keys(contacts)
-  
-    for (let index = 0; index < keys.length; index++) {
-        const key = keys[index];
-        let contactName = contacts[key].name
-        containerRef.innerHTML+=contactName
-    }
+let ordertContacts = [];
+
+async function initContacts() {
+  let contacts = await getData("contacts");
+  let keys = Object.keys(contacts);
+  for (let index = 0; index < keys.length; index++) {
+    const key = keys[index];
+    let contactName = contacts[key].name;
+    ordertContacts.push(contactName);
+    // containerRef.innerHTML += contactName;
+  }
+  console.log(ordertContacts);
+  ordertContacts.sort();
+  console.log(ordertContacts);
+  displayContacts();
+}
+
+function displayContacts() {
+  let containerRef = document.getElementById("contacts-container");
+  containerRef.innerHTML = "";
+  for (let i = 0; i < ordertContacts.length; i++) {
+    const contact = ordertContacts[i];
+    containerRef.innerHTML += contact;
+    console.log(contact);
+  }
 }
