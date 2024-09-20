b3a6d7e6c8995e07054f7e07393ea6a8a50f4644
Marc Schaar <m-schaar@gmx.net>
2024-09-20 12:30:21 +0200

    contact - first letter

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,5 @@
 let ordertContacts = [];
+let indexArray = [];
 
 async function initContacts() {
   await getContacts();
@@ -26,10 +27,25 @@ function displayContacts() {
   for (let i = 0; i < ordertContacts.length; i++) {
     const contactName = ordertContacts[i].name;
     const contactEmail = ordertContacts[i].email;
+    indexArray.push(i);
     containerRef.innerHTML += `<div class="d-flex contact">
-  <div class="user-circle center">AM</div>
+  <div id="contact-circle${i}" class="contact-circle center"></div>
   <span class="column"><span>${contactName}</span> <a href="mailto:${contactEmail}">${contactEmail}</a></span>
 </div>
 `;
+  }
+  getFirstLettersOfName();
+}
+
+function getFirstLettersOfName() {
+  for (let i = 0; i < indexArray.length; i++) {
+    let contactCirleRef = document.getElementById(`contact-circle${i}`);
+    const nameIndex = indexArray[i];
+    let fullName = ordertContacts[nameIndex].name;
+    let spliTName = fullName.split(" ");
+    let firstName = spliTName[0].charAt(0);
+    let LastName = spliTName[1].charAt(0);
+    console.log(firstName, LastName);
+    contactCirleRef.innerHTML = `${firstName + LastName}`;
   }
 }
