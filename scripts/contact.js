b86ba0acf69aa91017c97dc5c12dd5273ed04699
Marc Schaar <m-schaar@gmx.net>
2024-09-21 06:24:02 +0200

    random bg color

--- scripts/contact.js
+++ scripts/contact.js
@@ -80,5 +80,26 @@ function getFirstLettersOfName() {
     let lastName = spliTName[fullNameLength].charAt(0);
 
     contactCircleRef.innerHTML = `${firstName}${lastName}`;
+    updateCircleColor(i);
   }
 }
+function updateCircleColor(i) {
+  const contactCircleRef = document.getElementById(`contact-circle${i}`);
+  const rgb = [255, 0, 0];
+
+  rgb[0] = Math.round(Math.random() * 255);
+  rgb[1] = Math.round(Math.random() * 255);
+  rgb[2] = Math.round(Math.random() * 255);
+
+  const brightness = Math.round((parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) / 1000);
+  const textColour = brightness > 125 ? "black" : "white";
+  const backgroundColour = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+
+  contactCircleRef.style.backgroundColor = backgroundColour;
+  contactCircleRef.style.color = textColour;
+}
+
+// function updateCircleColor(i) {
+//   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+// }
