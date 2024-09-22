ff761c9b817ce082cf66f8dc2771bd072b4e7fa5
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-09-22 16:03:38 +0200

    show contacts details js

--- scripts/contact-details.js
+++ scripts/contact-details.js
@@ -1,12 +1,27 @@
 
-function getCurrentContactDetail() {
-    let contactsArray = getFromLocalStorage('contacts')
-    console.log(contactsArray);
-    
 
-}
 
 
 function showContact() {
-    let
+    let currentContact = document.getElementById('current-contact');
+    let detailsContainer = document.getElementById('details');
+    let detail = getCurrentContactDetail();
+
+    currentContact.innerHTML = `
+    <div class="current-contact-circle center" style="background:${detail.color}">${createInititals(detail.name)}</div>
+      <h2>${detail.name}</h2>
+    `
+
+    detailsContainer.innerHTML = `
+    <h3>Contact Information</h3>
+      <p class="bold">Email</p>
+      <a href="mailto:${detail.email}">${detail.email}</a>
+      <p class="bold">Phone</p>
+      <a href="tel:${detail.phone}">${detail.phone}</a>
+    `
+}
+
+function getCurrentContactDetail() {
+    let contactsArray = getFromLocalStorage('contacts');
+    return contactsArray[currentContactDetails];
 }
