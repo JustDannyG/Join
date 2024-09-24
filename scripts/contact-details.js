eb8109d66ab5a4b8482390d682d81ee81acdeb20
Marc Schaar <m-schaar@gmx.net>
2024-09-24 14:33:10 +0200

    templates

--- scripts/contact-details.js
+++ scripts/contact-details.js
@@ -6,18 +6,8 @@ function showContact() {
     let detailsContainer = document.getElementById('details');
     let detail = getCurrentContactDetail();
 
-    currentContact.innerHTML = `
-    <div class="current-contact-circle center" style="background:${detail.color}">${createInititals(detail.name)}</div>
-      <h2>${detail.name}</h2>
-    `
-
-    detailsContainer.innerHTML = `
-    <h3>Contact Information</h3>
-      <p class="bold">Email</p>
-      <a href="mailto:${detail.email}">${detail.email}</a>
-      <p class="bold">Phone</p>
-      <a href="tel:${detail.phone}">${detail.phone}</a>
-    `
+    currentContact.innerHTML = contactCirleHTML(detail);
+    detailsContainer.innerHTML = contactInformationsHTML(detail);
 }
 
 function getCurrentContactDetail() {
@@ -28,42 +18,29 @@ function getCurrentContactDetail() {
 function toggleOverlayDisplay() {
     let overlay = document.getElementById('edit-overlay-bg');
     overlay.classList.toggle('hide-overlay');
-    editDetails()
+    editDetails();
 }
 
 function editDetails() {
-
-  let currentDetail = getCurrentContactDetail();
-  document.getElementById('edit-name').value = currentDetail.name;
-  document.getElementById('edit-email').value = currentDetail.email;
-  document.getElementById('edit-phone').value = currentDetail.phone;
+    let currentDetail = getCurrentContactDetail();
+    document.getElementById('edit-name').value = currentDetail.name;
+    document.getElementById('edit-email').value = currentDetail.email;
+    document.getElementById('edit-phone').value = currentDetail.phone;
 
 }
 
-
-
 async function getCurrentKey() {
-
-  let allContacts = await getData(path = "/contacts");
-  console.log(allContacts);
-
-
+    let allContacts = await getData(path = "/contacts");
     let contactKeys = Object.keys(allContacts)
 
-    console.log(contactKeys);
     contactKeys.forEach((key, i) => {
-
-
-    console.log(key);
-    console.log(allContacts[key].name);
-    currentSortKeys.push({
-      'name': allContacts[key].name,
-      'key': key
-    })
-  });
-  sortByAlphabet(currentSortKeys);
-  console.log(currentSortKeys);
-  editContact()
+        currentSortKeys.push({
+            'name': allContacts[key].name,
+            'key': key
+        })
+    });
+    sortByAlphabet(currentSortKeys);
+    editContact()
 }
 
 
@@ -71,17 +48,17 @@ async function getCurrentKey() {
 
 
 function editContact() {
-  let key = currentSortKeys[currentContactDetails].key;
-  let name = document.getElementById('edit-name').value;
-  let email = document.getElementById('edit-email').value;
-  let phone = document.getElementById('edit-phone').value;
-  let contactsArray = getFromLocalStorage('contacts');
-  putData(path = `/contacts/${key}`, data = {
+    let key = currentSortKeys[currentContactDetails].key;
+    let name = document.getElementById('edit-name').value;
+    let email = document.getElementById('edit-email').value;
+    let phone = document.getElementById('edit-phone').value;
+    let contactsArray = getFromLocalStorage('contacts');
+    putData(path = `/contacts/${key}`, data = {
 
-    'color': contactsArray[currentContactDetails].color,
-    'name': name,
-    'email': email,
-    'phone': phone
+        'color': contactsArray[currentContactDetails].color,
+        'name': name,
+        'email': email,
+        'phone': phone
 
-  })
+    })
 }
