f71a694381889f67973171505d1244c15b3b9074
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-10-12 20:35:24 +0200

    update contact detail

--- assets/templates/template.js
+++ assets/templates/template.js
@@ -109,34 +109,37 @@ function generateNoTaskHTML(noTask) {
 //Templates for Contact-Details
 function contactCirleHTML(detail) {
     return /*html*/ `
-    <div class="current-contact-circle center" style="background:${detail.color}">
-        ${createInititals(detail.name)}
+    <div class="detail-container">
+        <div class="contact-name-details">
+            <span class="name-initals" style="background:${detail.color}">${createInititals(detail.name)}</span>
+            <div class="name-detail">
+                ${detail.name}
+                <div class="edit-details-btns">
+                    <button onclick="classChangeAction('edit-overlay-bg', 'hide-overlay-desktop', 'remove'); editDetails();"><img src="./assets/icons/edit.png"/>Edit</button>
+                    <button onclick="deleteContact()"><img src="./assets/icons/delete.png" />Delete</button>
+                </div>
+            </div>
+        </div>
+        <p class="info-headline">Contact Information</p>
+        <div class="contact-info-box">
+            <span>Email</span>
+            <a href="mailto:${detail.email}">${detail.email}</a>
+            <span>Phone</span>
+            <a href="tel:${detail.phone}">${detail.phone}</a>
+        </div>
     </div>
-    <div class="column">
-      <h2>${detail.name}</h2>
-      <div class="edit-contact-desktop">
-        <button class="edit-contact-btn" onclick="classChangeAction('edit-overlay-bg', 'hide-overlay-desktop', 'remove'); editDetails();">
-            <img src="./assets/icons/edit.png " alt="Edit Button" /> Edit
-        </button>
-
-        <button class="edit-contact-btn" onclick="deleteContact()">
-            <img src="./assets/icons/delete.png " alt="Delete Button" /> Delete
-      </button>
-    </div>
-
-      </div>
     `;
 }
 
-function contactInformationsHTML(detail) {
-    return /*html*/ `
-    <h3>Contact Information</h3>
-      <p class="bold">Email</p>
-      <a href="mailto:${detail.email}">${detail.email}</a>
-      <p class="bold">Phone</p>
-      <a href="tel:${detail.phone}">${detail.phone}</a>
-    `;
-}
+// function contactInformationsHTML(detail) {
+//     return /*html*/ `
+//     <h3>Contact Information</h3>
+//       <p class="bold">Email</p>
+//       <a href="mailto:${detail.email}">${detail.email}</a>
+//       <p class="bold">Phone</p>
+//       <a href="tel:${detail.phone}">${detail.phone}</a>
+//     `;
+// }
 
 //Templates for Contact List
 function firstLetterHtml(firstLetter) {
