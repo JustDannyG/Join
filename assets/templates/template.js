069070f87ae4248a2e05c8a1573a061e5d2eec9b
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2024-10-12 18:57:17 +0200

    clean

--- assets/templates/template.js
+++ assets/templates/template.js
@@ -67,6 +67,87 @@ function desktopSidebar() {
     </aside>`;
 }
 
+
+////////////////////////////////////////////////////
+/////       Add-Task - Subtask Templates      /////
+//////////////////////////////////////////////////
+
+function subtaskBtnHTML() {
+    return `
+        <div class="subtasks-btns">
+
+         <div class="svg-btn" onclick="clearSubtask()"> 
+            <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path d="M6.24959 6.99984L11.4926 12.2428M1.00659 12.2428L6.24959 6.99984L1.00659 12.2428ZM11.4926 1.75684L6.24859 6.99984L11.4926 1.75684ZM6.24859 6.99984L1.00659 1.75684L6.24859 6.99984Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+           </svg>
+        </div>
+
+           <span class="break-line"></span>
+
+           <div class="svg-btn" onclick="addSubtask()">
+            <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path d="M5.79923 9.15L14.2742 0.675C14.4742 0.475 14.7117 0.375 14.9867 0.375C15.2617 0.375 15.4992 0.475 15.6992 0.675C15.8992 0.875 15.9992 1.1125 15.9992 1.3875C15.9992 1.6625 15.8992 1.9 15.6992 2.1L6.49923 11.3C6.29923 11.5 6.0659 11.6 5.79923 11.6C5.53256 11.6 5.29923 11.5 5.09923 11.3L0.79923 7C0.59923 6.8 0.503397 6.5625 0.51173 6.2875C0.520064 6.0125 0.62423 5.775 0.82423 5.575C1.02423 5.375 1.26173 5.275 1.53673 5.275C1.81173 5.275 2.04923 5.375 2.24923 5.575L5.79923 9.15Z" fill="black"/>
+           </svg>
+        </div>
+          
+        </div>
+`;
+}
+
+
+function subtaskTaskHTML(subtask, i) {
+    return `
+        <div id="subtask${i}" class="subtask">
+           <div class="subtask-text" onclick="editWord(${i})">
+             <p>${subtask.sub}</p> 
+             <img class="edit-icon" src="./assets/icons/edit.png" alt="">
+           </div>
+           <span class="break-line"></span>
+            <img class="edit-icon" src="./assets/icons/delete.png" alt="" onclick="deleteSubtask(${i})">
+        </div>
+        `;
+}
+
+
+function editIconsHTML(i) {
+    return `<div class="word-item">
+                <input type="text" id="editInput${i}" value="${currentSubtasks[i].sub}">
+                <button  onclick="deleteSubtask(${i})"><img src="./assets/icons/delete.png" alt=""></button>
+                <span class="break-line"></span>
+                <svg onclick="saveWord(${i})" width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.79911 9.15L14.2741 0.675C14.4741 0.475 14.7116 0.375 14.9866 0.375C15.2616 0.375 15.4991 0.475 15.6991 0.675C15.8991 0.875 15.9991 1.1125 15.9991 1.3875C15.9991 1.6625 15.8991 1.9 15.6991 2.1L6.49911 11.3C6.29911 11.5 6.06578 11.6 5.79911 11.6C5.53245 11.6 5.29911 11.5 5.09911 11.3L0.799113 7C0.599113 6.8 0.50328 6.5625 0.511613 6.2875C0.519946 6.0125 0.624113 5.775 0.824113 5.575C1.02411 5.375 1.26161 5.275 1.53661 5.275C1.81161 5.275 2.04911 5.375 2.24911 5.575L5.79911 9.15Z" fill="black"/>
+                 </svg>
+                
+            </div>`;
+}
+
+
+/////////////////////////////////////////////
+/////       Add-Task - Templates       /////
+///////////////////////////////////////////
+
+function contactInDropDownHTML(contact, initials) {
+    return /*html*/ `
+            <li id="contact${contact.id}" onclick="selectContact(${contact.id}); stopEventBubbling(event)">
+                <div class="d-flex contact-row">
+                    <div class="center gap">
+                        <div class="contact center" style="background-color:${contact.color}">${initials}</div>
+                        <span>${contact.name}</span>
+                    </div>
+                    <div class="container">
+                        <input type="checkbox" id="checkbox${contact.id}" onclick="selectContact('${contact.name}'); stopEventBubbling(event)">
+                        <span class="checkmark"></span>
+                    </div>
+                </div>
+            </li>`;
+}
+
+
+function contactSelectionCircleHTML(contact, initials) {
+    return /*html*/ `<div class="contact center " style="background-color:${contact.color}">${initials}</div>`;
+}
+
+
 ////////////////////////////////////////////
 /////  Board  -  Tasks  Templates     //////
 ///////////////////////////////////////////
@@ -98,136 +179,14 @@ function generateTaskHTML(task, index, className) {
               </div> `;
 }
 
+
 function generateNoTaskHTML(noTask) {
     return /*html*/ `<div class="no-task"> No task in ${noTask}</div> `;
 }
 
-////////////////////////////////////////////
-/////      Contact - Templates       //////
-///////////////////////////////////////////
-
-//Templates for Contact-Details
-function contactCirleHTML(detail) {
-    return /*html*/ `
-    <div class="current-contact-circle center" style="background:${detail.color}">
-        ${createInititals(detail.name)}
-    </div>
-    <div class="column">
-      <h2>${detail.name}</h2>
-      <div class="edit-contact-desktop">
-        <button class="edit-contact-btn" onclick="classChangeAction('edit-overlay-bg', 'hide-overlay-desktop', 'remove'); editDetails();">
-            <img src="./assets/icons/edit.png " alt="Edit Button" /> Edit
-        </button>
-
-        <button class="edit-contact-btn" onclick="deleteContact()">
-            <img src="./assets/icons/delete.png " alt="Delete Button" /> Delete
-      </button>
-    </div>
-
-      </div>
-    `;
-}
-
-function contactInformationsHTML(detail) {
-    return /*html*/ `
-    <h3>Contact Information</h3>
-      <p class="bold">Email</p>
-      <a href="mailto:${detail.email}">${detail.email}</a>
-      <p class="bold">Phone</p>
-      <a href="tel:${detail.phone}">${detail.phone}</a>
-    `;
-}
-
-//Templates for Contact List
-function firstLetterHtml(firstLetter) {
-    return /*html*/ `<div class="contacts-first-letter">${firstLetter}</div>`;
-}
-
-function contactListHtml(contact, i) {
-    return /*html*/ `<div onclick="openContact(${i})" class="contact-list d-flex">
-      <span class="contact-initials center" style="background:${contact.color}">${createInititals(contact.name)}</span>
-      <div>
-        <p>${contact.name}</p>
-        <a href="#">${contact.email}</a>
-      </div>
-    </div>`;
-}
-
-//Templates for Add Task
-
-function contactInDropDownHTML(contact, initials) {
-    return /*html*/ `
-            <li id="contact${contact.id}" onclick="selectContact(${contact.id}); stopEventBubbling(event)">
-                <div class="d-flex contact-row">
-                    <div class="center gap">
-                        <div class="contact center" style="background-color:${contact.color}">${initials}</div>
-                        <span>${contact.name}</span>
-                    </div>
-                    <div class="container">
-                        <input type="checkbox" id="checkbox${contact.id}" onclick="selectContact('${contact.name}'); stopEventBubbling(event)">
-                        <span class="checkmark"></span>
-                    </div>
-                </div>
-            </li>`;
-}
-
-function contactSelectionCircleHTML(contact, initials) {
-    return /*html*/ `<div class="contact center " style="background-color:${contact.color}">${initials}</div>`;
-}
-
-////////////////////////////////////////////////////
-/////       Add-Task - Subtask Templates      /////
-//////////////////////////////////////////////////
-
-function subtaskBtnHTML() {
-    return `
-        <div class="subtasks-btns">
-
-         <div class="svg-btn" onclick="clearSubtask()"> 
-            <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-             <path d="M6.24959 6.99984L11.4926 12.2428M1.00659 12.2428L6.24959 6.99984L1.00659 12.2428ZM11.4926 1.75684L6.24859 6.99984L11.4926 1.75684ZM6.24859 6.99984L1.00659 1.75684L6.24859 6.99984Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-           </svg>
-        </div>
-
-           <span class="break-line"></span>
-
-           <div class="svg-btn" onclick="addSubtask()">
-            <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-             <path d="M5.79923 9.15L14.2742 0.675C14.4742 0.475 14.7117 0.375 14.9867 0.375C15.2617 0.375 15.4992 0.475 15.6992 0.675C15.8992 0.875 15.9992 1.1125 15.9992 1.3875C15.9992 1.6625 15.8992 1.9 15.6992 2.1L6.49923 11.3C6.29923 11.5 6.0659 11.6 5.79923 11.6C5.53256 11.6 5.29923 11.5 5.09923 11.3L0.79923 7C0.59923 6.8 0.503397 6.5625 0.51173 6.2875C0.520064 6.0125 0.62423 5.775 0.82423 5.575C1.02423 5.375 1.26173 5.275 1.53673 5.275C1.81173 5.275 2.04923 5.375 2.24923 5.575L5.79923 9.15Z" fill="black"/>
-           </svg>
-        </div>
-          
-        </div>
-`;
-}
-
-function subtaskTaskHTML(subtask, i) {
-    return `
-        <div id="subtask${i}" class="subtask">
-           <div class="subtask-text" onclick="editWord(${i})">
-             <p>${subtask.sub}</p> 
-             <img class="edit-icon" src="./assets/icons/edit.png" alt="">
-           </div>
-           <span class="break-line"></span>
-            <img class="edit-icon" src="./assets/icons/delete.png" alt="" onclick="deleteSubtask(${i})">
-        </div>
-        `;
-}
-
-function editIconsHTML(i) {
-    return `<div class="word-item">
-                <input type="text" id="editInput${i}" value="${currentSubtasks[i].sub}">
-                <button  onclick="deleteSubtask(${i})"><img src="./assets/icons/delete.png" alt=""></button>
-                <span class="break-line"></span>
-                <svg onclick="saveWord(${i})" width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5.79911 9.15L14.2741 0.675C14.4741 0.475 14.7116 0.375 14.9866 0.375C15.2616 0.375 15.4991 0.475 15.6991 0.675C15.8991 0.875 15.9991 1.1125 15.9991 1.3875C15.9991 1.6625 15.8991 1.9 15.6991 2.1L6.49911 11.3C6.29911 11.5 6.06578 11.6 5.79911 11.6C5.53245 11.6 5.29911 11.5 5.09911 11.3L0.799113 7C0.599113 6.8 0.50328 6.5625 0.511613 6.2875C0.519946 6.0125 0.624113 5.775 0.824113 5.575C1.02411 5.375 1.26161 5.275 1.53661 5.275C1.81161 5.275 2.04911 5.375 2.24911 5.575L5.79911 9.15Z" fill="black"/>
-                 </svg>
-                
-            </div>`;
-}
 
 //////////////////////////////////////////////////
-/////          Board Task Overlays          //////
+/////        Board - Task Overlays          //////
 //////////////////////////////////////////////////
 
 function taskBoardOverlay(currentTask) {
@@ -273,6 +232,7 @@ function taskBoardOverlay(currentTask) {
     </div>`;
 }
 
+
 function generateAssignedToOerlayLiHTML(contact) {
     return `
                         <li class="assigned-to-contact">
@@ -281,8 +241,9 @@ function generateAssignedToOerlayLiHTML(contact) {
                         </li>`;
 }
 
+
 //////////////////////////////////////////////////
-/////         Board Task Overlays Edit      //////
+/////       Board - Task Overlays Edit      //////
 //////////////////////////////////////////////////
 
 function editBoardTaskHTML(currentTask) {
@@ -362,5 +323,62 @@ function editBoardTaskHTML(currentTask) {
             
         </form>
         
+    </div>`;
+}
+
+
+////////////////////////////////////////////
+/////      Contact - Templates       //////
+///////////////////////////////////////////
+
+//Templates for Contact-Details
+function contactCirleHTML(detail) {
+    return /*html*/ `
+    <div class="current-contact-circle center" style="background:${detail.color}">
+        ${createInititals(detail.name)}
+    </div>
+    <div class="column">
+      <h2>${detail.name}</h2>
+       <div class="edit-contact-desktop">
+
+         <button class="edit-contact-btn" onclick="classChangeAction('edit-overlay-bg', 'hide-overlay-desktop', 'remove'); editDetails();">
+            <img src="./assets/icons/edit.png " alt="Edit Button" /> Edit
+         </button>
+
+         <button class="edit-contact-btn" onclick="deleteContact()">
+            <img src="./assets/icons/delete.png " alt="Delete Button" /> Delete
+         </button>
+       </div>
+
+    </div>
+
+    `;
+}
+
+
+function contactInformationsHTML(detail) {
+    return /*html*/ `
+    <h3>Contact Information</h3>
+      <p class="bold">Email</p>
+      <a href="mailto:${detail.email}">${detail.email}</a>
+      <p class="bold">Phone</p>
+      <a href="tel:${detail.phone}">${detail.phone}</a>
+    `;
+}
+
+
+//Templates for Contact List
+function firstLetterHtml(firstLetter) {
+    return /*html*/ `<div class="contacts-first-letter">${firstLetter}</div>`;
+}
+
+
+function contactListHtml(contact, i) {
+    return /*html*/ `<div onclick="openContact(${i})" class="contact-list d-flex">
+      <span class="contact-initials center" style="background:${contact.color}">${createInititals(contact.name)}</span>
+      <div>
+        <p>${contact.name}</p>
+        <a href="#">${contact.email}</a>
+      </div>
     </div>`;
 }
